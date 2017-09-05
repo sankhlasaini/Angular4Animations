4abf5c5034d2c1fc57c98801fd893a3eb18c4f12
Divesh Sankhla <sankhlasaini@gmail.com>
2017-09-06 00:38:32 +0530

    angular 2 animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { transition, trigger, query, stagger, animate, state, style, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('flyInOut', [
+      transition(':enter', [
+        animate(100, keyframes([
+          style({ opacity: 0, transform: 'translateY(5px)', offset: 0 }),
+          style({ opacity: 1, transform: 'translateY(0)', offset: 1.0 })
+        ]))
+      ])
+    ])
+  ]
 })
 export class AppComponent {
+  count = 0;
+  showArray = [];
+
   title = 'app';
+  heroes = ['Alpha', 'Bravo', 'Charlie', 'Delta',
+    'Echo', 'Foxtrot', 'Golf', 'Hotel', 'India', 'Juliet', 'Kilo',
+    'Lima', 'Mike', 'November', 'Oscar', 'Papa', 'Qubec', 'Romio', 'Sierra'];
+
+  constructor() {
+    this.pushData();
+  }
+
+  pushData() {
+    if (this.count < 7) {
+      this.showArray.push(this.heroes[this.count]);
+      this.count++;
+    }
+  }
+   pushData2() {
+      this.showArray.push(this.heroes[this.count]);
+      this.count++;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
+import { MaterialModule, MdNativeDateModule, DateAdapter, NativeDateAdapter } from '@angular/material';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    FormsModule,
+    HttpModule,
+    ReactiveFormsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
