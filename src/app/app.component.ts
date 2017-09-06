f541f634a71fd5971c64bdf2f38572d9b81d8a05
Divesh Sankhla <sankhlasaini@gmail.com>
2017-09-06 22:12:33 +0530

    selection list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,11 +19,16 @@ import { transition, trigger, query, stagger, animate, state, style, keyframes }
 export class AppComponent {
   count = 0;
   showArray = [];
-
+  asd = false;
+  selectAll = false;
+  selectAllIcon = { icon: 'check_box_outline_blank', color: 'gray' };
   title = 'app';
-  heroes = ['Alpha', 'Bravo', 'Charlie', 'Delta',
-    'Echo', 'Foxtrot', 'Golf', 'Hotel', 'India', 'Juliet', 'Kilo',
-    'Lima', 'Mike', 'November', 'Oscar', 'Papa', 'Qubec', 'Romio', 'Sierra'];
+  heroes = [
+    { name: '123123', tick: true },
+    { name: '5657', tick: true },
+    { name: 'trhfth', tick: false },
+    { name: '1231fthdas23', tick: true },
+  ];
 
   constructor() {
     this.pushData();
@@ -35,9 +40,49 @@ export class AppComponent {
       this.count++;
     }
   }
-   pushData2() {
-      this.showArray.push(this.heroes[this.count]);
-      this.count++;
+  pushData2() {
+    this.showArray.push(this.heroes[this.count]);
+    this.count++;
+    console.log(this.heroes);
   }
+
+  toggle(item) {
+    if (item.tick) {
+      item.tick = false;
+    } else {
+      item.tick = true;
+    }
+    let count = 0;
+    this.heroes.forEach(element => {
+      if (element.tick) {
+        count++;
+      }
+    });
+    if (count === 0) {
+      this.selectAllIcon = { icon: 'check_box_outline_blank', color: 'gray' };
+    } else if (count === this.heroes.length) {
+      this.selectAllIcon = { icon: 'check_box', color: '' };
+    } else {
+      this.selectAllIcon = { icon: 'indeterminate_check_box', color: '' };
+    }
+  }
+
+
+  selectAllfunc() {
+    if (this.selectAll) {
+      this.selectAll = false;
+      this.selectAllIcon = { icon: 'check_box_outline_blank', color: 'gray' };
+      this.heroes.forEach(element => {
+        element.tick = false;
+      });
+    } else {
+      this.selectAll = true;
+      this.selectAllIcon = { icon: 'check_box', color: '' };
+      this.heroes.forEach(element => {
+        element.tick = true;
+      });
+    }
+  }
+
 
 }
